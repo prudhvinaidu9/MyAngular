b5580fe65198783e6dde41c4bc42e032c9e8a69d
Prudhvi Naidu <prudhvi.guddati555@gmail.com>
2020-08-12 19:34:54 +0530

    done with all files

---
+++ core-angular/src/app/card.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "card-item",
+    templateUrl: "./card.component.html",
+    styleUrls: ["./card.component.css"]
+})
+
+export class CardComponent{}
